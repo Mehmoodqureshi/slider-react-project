d56114d09674cd55704f82d91dc6cf50359b9c01
mehmood qureshi <mehmoodqureshi997@gmail.com>
2021-09-13 04:13:54 -0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,49 @@
+const people = [
+  {
+    id: 1,
+    name: "Hamzah Ejaz",
+    title: "web developer",
+    image:
+      "https://scontent.fisb4-1.fna.fbcdn.net/v/t1.6435-9/72537971_2431193120333360_1660175541904343040_n.jpg?_nc_cat=100&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGATQMeP8UysmcnBe3zfXn1YV6WWVo2jmdhXpZZWjaOZzOMzH5Wd_ooFzPI1Hflb2RBlTE2o1cu-lDkRhiRooHa&_nc_ohc=h31IqfUj0xQAX_OaDue&_nc_ht=scontent.fisb4-1.fna&oh=4e83deae61174530833f7f8ec006d48a&oe=61481AD7",
+    quote:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "Hamza Ali",
+    title: "web designer",
+    image:
+      "https://scontent.fisb4-1.fna.fbcdn.net/v/t1.6435-9/41289762_1403488889783572_2118084193131954176_n.jpg?_nc_cat=101&ccb=1-5&_nc_sid=174925&_nc_eui2=AeEk_uiYXwVlmNXXvSok8AQcVqqmV0CbzNVWqqZXQJvM1X_YS57UNZ3GM_GUd8ZwhUk0caTlr62_gxF4IOtGPI6L&_nc_ohc=K4Ob5Lb5gQ4AX-OL_Ia&_nc_ht=scontent.fisb4-1.fna&oh=ab628f37b55e5b65139849eeed2a2c31&oe=614AD156",
+    quote:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "Saqlain Mushtaq",
+    title: "Intern",
+    image:
+      "https://scontent.fisb4-2.fna.fbcdn.net/v/t1.6435-9/217849969_1385977371774289_2750261365852592229_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeEVLYsjXCikAX9ou1y2k2nGYeLITgNFYXRh4shOA0VhdPeMNhx3sfo7AbEx5QQs6JEGvQ_2X8boHMRan_Ofdivs&_nc_ohc=9HX7aYXKjqQAX-ydPYh&_nc_ht=scontent.fisb4-2.fna&oh=598c2668458dc15070829a7f31d2a203&oe=6149E77E",
+    quote:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "Junaid Jamshaid",
+    title: "the boss",
+    image:
+      "https://scontent.fisb4-2.fna.fbcdn.net/v/t1.6435-1/s200x200/143246879_2729292653986648_4610385476382202819_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=7206a8&_nc_eui2=AeHFWs8ZTCoNwfFMLYZdEoyxQKmjyLPX8o9AqaPIs9fyjz9Sre7AKRzIISgGkssW7wuxEb4ZVqHzZcLeJDdw3Ewp&_nc_ohc=FCcL6QzjsU4AX8m_1L_&tn=3RiDZPByEQdLQFws&_nc_ht=scontent.fisb4-2.fna&oh=098e48a5fa649e598f25bb0bcf193b39&oe=614BFF87",
+    quote:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+  {
+    id: 5,
+    name: "Mehmood Qureshi",
+    title: "the boss",
+    image:
+      "https://scontent.fisb4-1.fna.fbcdn.net/v/t1.6435-1/c0.0.160.160a/p160x160/220772534_1946683555483731_2889679522473614326_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=7206a8&_nc_eui2=AeHWQxycOjB0Ii5YeDBwrzjSSoPfDr6VVRBKg98OvpVVEAXUdJCgBiMLlawtWG_QKgq9M4HlBQ6uo-_itvhqXvqJ&_nc_ohc=ndMFTqJYY6wAX_UDvkd&tn=3RiDZPByEQdLQFws&_nc_ht=scontent.fisb4-1.fna&oh=55f626e51b6aa1b3055c0f45004103ad&oe=614B0A95",
+    quote:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+export default people;
